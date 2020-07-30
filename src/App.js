a6cc30d878cd57897b1c4787fcfb9a8fdb3fb1a9
Nick Macinic <nickmacinic@gmail.com>
2020-07-30 14:55:28 +0100

    Store posts in the database

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
+import { db } from "./firebase";
 
+//use effect runs a piece of code on a specific condition
 function App() {
-  const [posts, setPosts] = useState([
-    {
-    username :"Nick",
-    caption :"Yes Sirrrrrr it works imma be rich",
-    imageUrl :"https://betterstudio.com/wp-content/uploads/2019/05/1-1-instagram-1024x1024.jpg"
-    },
-    {
-      username: "Xenia",
-      caption: "This is my dog and i love him so much yes",
-      imageUrl: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg?resize=750px:*"
-    }
-  ])
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    //this is where code runs
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="app__header">
-       <img
+        <img
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt="logo"
         />
       </div>
       <h1>Hello</h1>
-      {
-        posts.map(post => (
-          <Post username ={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
-        ))
-      }
+      {posts.map(({ id, post }) => (
+        <Post
+          key
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
